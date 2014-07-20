13238ae6f061e523dbcff62c0b88254569bd0541
hame22 <alex.hamer@gmail.com>
2014-07-20 11:39:55 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express'),
+config = require('./lib/configuration'),
+routes = require('./lib/routes');
+
+var app = express();
+
+app.set('port', config.get('server:port'));
+console.log(app.get('port'));
+
+app.get('/', routes.index);
+
+app.listen(app.get('port'), function () {
+	console.log('Listening....');
+});
+
+module.exports = app;
