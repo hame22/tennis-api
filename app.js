536dbe863b055510940e82cf8af4efddb1df19a9
hame22 <alex.hamer@gmail.com>
2014-07-23 20:30:41 +0100

    Updated head to head stub

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('port', config.get('server:port'));
 console.log(app.get('port'));
 
 app.get('/allPlayers', routes.allPlayers);
+app.get('/headToHead', routes.headToHead)
 
 app.listen(app.get('port'), function () {
 	console.log('Listening....');
